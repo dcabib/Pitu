5accdce9311487c979030eb7db7b412e675db261
Daniel <danielsaraldi@gmail.com>
2020-11-24 12:34:09 -0300

    Added cors.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,5 +5,5 @@ import database from './database'
 database.sync()
 console.log('Database running at 3306')
 
-app.listen(3333)
-console.log('Server running at 3333')
+app.listen(3001)
+console.log('Server running at 3001')
